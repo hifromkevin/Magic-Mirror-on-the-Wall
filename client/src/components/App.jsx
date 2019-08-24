649eef6adff9f7c426bc14426c0f52f6ec710529
hifromkevin <lam.kevin86@gmail.com>
2019-08-24 12:52:14 -0700

    Adds components and passes example headlines and weather

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,10 +1,44 @@
 import React, { Component } from 'react';
 
+import DateAndTime from './DateAndTime.jsx';
+import Headlines from './Headlines.jsx';
+import Weather from './Weather.jsx';
+import WelcomeText from './WelcomeText.jsx';
+
 export default class App extends Component {
 	constructor(props) {
 		super(props);
 
 		this.state = {
+			weatherToday: {
+					weather: 'Sunny',
+					high: '102',
+					low: '82'
+			},
+			forecasts: [
+				{
+					day: 'Monday',
+					weather: 'Sunny',
+					high: '98',
+					low: '72'
+				},
+				{
+					day: 'Tuesday',
+					weather: 'Rainy',
+					high: '62',
+					low: '48'
+				}
+			],
+			headlines: [
+				{
+					title: 'Corduroy Pillows', 
+					author: 'James Brown'
+				},
+				{
+					title: 'Drawing on Your Face With Sharpies', 
+					author: 'Bob Ross'
+				}
+			]
 		}
 	}
 	
@@ -13,14 +47,23 @@ export default class App extends Component {
 			<div className="main">
 				<div className="top">
 					<p>Top</p>
+					<Weather 
+						weatherToday = {this.state.weatherToday}
+						forecasts = {this.state.forecasts} 
+					/>
+					<DateAndTime />
 					<div className="clear"></div>
 				</div>
 				<div className="middle">
 					<p>Middle</p>
+					<WelcomeText />
 					<div className="clear"></div>
 				</div>
 				<div className="bottom">
 					<p>Bottom</p>
+					<Headlines 
+						headlines = {this.state.headlines} 
+					/>
 					<div className="clear"></div>
 				</div>
 			</div>
