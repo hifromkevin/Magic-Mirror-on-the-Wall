835a61b328e98aedaf4f754ef5fbbc48c1c326d4
hifromkevin <lam.kevin86@gmail.com>
2019-08-24 11:32:28 -0700

    Adds webpack configuration

---
+++ webpack.config.js
@@ -0,0 +1,35 @@
+const path = require('path');
+const DIST_DIR = path.join(__dirname, '/client/dist'); 
+const SRC_DIR = path.join(__dirname, '/client/src');
+
+module.exports = {
+	mode: 'development',
+	entry: `${SRC_DIR}/index.jsx`,
+	output: {
+		path: DIST_DIR,
+		filename: 'bundle.js'
+	},
+	module: {
+		rules: [
+			{
+				test: /\.jsx$/,
+				loader: 'babel-loader',
+				query: {
+					presets: ['@babel/preset-env', '@babel/preset-react']
+				}
+			},
+			{
+				test: /\.sass$/,
+				loaders: ['style-loader','css-loader','sass-loader']
+			},
+      {
+        test: /\.css$/,
+        loaders: ['style-loader','css-loader','sass-loader']
+      },
+      {
+        test: /\.(png|woff|woff2|eot|ttf|svg)$/,
+        loader: 'url-loader?limit=100000'
+      }
+		]
+	}
+};
