529314b5e31d4e64e5f72057136082bf3a63dc15
hifromkevin <lam.kevin86@gmail.com>
2019-08-24 18:49:43 -0700

    Updates weather to get state from App

--- client/src/components/Weather.jsx
+++ client/src/components/Weather.jsx
@@ -4,11 +4,16 @@ import Forecasts from './Forecasts.jsx';
 
 const Weather = ({ weatherToday, forecasts }) => (
 		<div className="weather">
-			<h1>Weather</h1>
-				<h2 className="weather__temperature">
-					<span className="weather__low">{weatherToday.low}</span> / <span className="weather__high">{weatherToday.high}</span>
-				</h2>
-			<p>{weatherToday.weather}</p>
+			<p className="weather__temperature"><span className="weather__low">{weatherToday.low}</span> / <span className="weather__high">{weatherToday.high}</span></p>
+			<img 
+				src="img/sunny.png" 
+				alt="sunny" 
+				className="weather__icon" 
+			/>
+			<p className="weather__location">in {weatherToday.location}</p>			
+			<p className="weather__type">{weatherToday.weather}</p>
+			<p className="weather__description">{weatherToday.description}</p>
+			
 			<Forecasts forecasts={forecasts} />
 		</div>
 )
