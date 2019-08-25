5245a783f6150a8063f54204e22e894bc8284a0e
hifromkevin <lam.kevin86@gmail.com>
2019-08-24 17:53:09 -0700

    Switches DateAndTime from functional to class component

--- client/src/components/DateAndTime.jsx
+++ client/src/components/DateAndTime.jsx
@@ -1,7 +1,20 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-const DateAndTime = () => (
-		<h1>DateAndTime</h1>
-)
+export default class DateAndTime extends Component {
+	constructor(props) {
+		super(props);
 
-export default DateAndTime;
+		this.state = {
+			months:['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'],
+			days:['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']
+		}
+	}
+
+	render() { 
+		return (
+			<div className="dateAndTime">
+				<h1>Date and Time</h1>
+			</div>
+		);
+	}
+};
