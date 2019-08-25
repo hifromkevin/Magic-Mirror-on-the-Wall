1aa40962dc4cfd214574e1bb5b712bd4f2dfeb60
hifromkevin <lam.kevin86@gmail.com>
2019-08-24 18:07:21 -0700

    Adds realtime date and time

--- client/src/components/DateAndTime.jsx
+++ client/src/components/DateAndTime.jsx
@@ -8,12 +8,62 @@ export default class DateAndTime extends Component {
 			months:['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'],
 			days:['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']
 		}
+
+		this.realtimeDateAndTime = this.realtimeDateAndTime.bind(this);
+		this.getTime = this.getTime.bind(this);
+		this.getHour = this.getHour.bind(this);
+	}
+
+	UNSAFE_componentWillMount() {
+		this.realtimeDateAndTime();
+	}
+
+	realtimeDateAndTime() {
+		let currentDate = new Date();
+
+		this.setState({
+			date: currentDate,
+			seconds: this.lessThanTen(currentDate.getSeconds()),
+			minutes: this.lessThanTen(currentDate.getMinutes()),
+			hours: this.getHour(currentDate.getHours()), 
+			day: currentDate.getDate(),
+			dayOfTheWeek: this.state.days[currentDate.getDay()],
+			month: this.state.months[currentDate.getMonth()],
+			year: currentDate.getFullYear()
+		});
+
+		setTimeout(this.realtimeDateAndTime, 1000);
+	}
+
+	getTime() {
+		let second = this.state.seconds;
+		let minute = this.state.minutes;
+		let hour = this.state.hours;
+
+		let timeOfDay = (hour < 12) ? 'AM' : 'PM';
+
+		return `${hour}:${minute}:${second} ${timeOfDay}`;
+	}
+
+	getHour(n) {
+		if (n > 12) {
+			n = n - 12;
+		} else if (n === 0) {
+			n = 12;
+		} 
+
+		return n;
+	};
+
+	lessThanTen(n) {
+		return (n < 10) ? ('0' + n) : n;
 	}
 
 	render() { 
 		return (
 			<div className="dateAndTime">
-				<h1>Date and Time</h1>
+				<p className="dateAndTime__time">{this.getTime()}</p>
+				<p className="dateAndTime__date">{this.state.dayOfTheWeek}<br /> {this.state.month} {this.state.day}, {this.state.year}</p>
 			</div>
 		);
 	}
