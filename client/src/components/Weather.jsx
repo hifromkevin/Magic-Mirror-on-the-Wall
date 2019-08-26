4330bce316cf228a45ba475b13a8a73fa7025597
hifromkevin <lam.kevin86@gmail.com>
2019-08-25 19:03:02 -0700

    Updated to pass new state to Forecasts

--- client/src/components/Weather.jsx
+++ client/src/components/Weather.jsx
@@ -2,19 +2,20 @@ import React from 'react';
 
 import Forecasts from './Forecasts.jsx';
 
-const Weather = ({ weatherToday, forecasts }) => (
+const Weather = ({ weatherToday, forecasts, weatherIcons }) => (
 		<div className="weather">
 			<p className="weather__temperature"><span className="weather__low">{weatherToday.low}</span> / <span className="weather__high">{weatherToday.high}</span></p>
+			{console.log(weatherIcons[weatherToday.weather])}
 			<img 
-				src="img/sunny.png" 
-				alt="sunny" 
+				src={weatherIcons[weatherToday.weather]} 
+				alt={weatherToday.weather} 
 				className="weather__icon" 
 			/>
 			<p className="weather__location">in {weatherToday.location}</p>			
 			<p className="weather__type">{weatherToday.weather}</p>
 			<p className="weather__description">{weatherToday.description}</p>
-			
-			<Forecasts forecasts={forecasts} />
+
+			<Forecasts forecasts={forecasts} weatherIcons={weatherIcons} />
 		</div>
 )
 
