088bfb21276c640400ec1b9a1164724ef20dce8f
hifromkevin <lam.kevin86@gmail.com>
2019-08-25 19:02:38 -0700

    Adds weather icon feature

--- client/src/components/Forecasts.jsx
+++ client/src/components/Forecasts.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
 import SingleForecast from './SingleForecast.jsx';
 
-const Forecasts = ({ forecasts }) => (
+const Forecasts = ({ forecasts, weatherIcons }) => (
 	<div className="forecasts">
 		<h1 className="forecasts__title">Forecast</h1>
-		{forecasts.map((forecast, i) => <SingleForecast forecast={forecast} key={i} />)}
+		{forecasts.map((forecast, i) => <SingleForecast forecast={forecast} weatherIcons={weatherIcons} key={i} />)}
 	</div>
 )
 
