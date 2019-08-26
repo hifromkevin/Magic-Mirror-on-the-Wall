66b058affcbffe426ed4321e9c050aa08d1248f9
hifromkevin <lam.kevin86@gmail.com>
2019-08-25 19:03:26 -0700

    Adds icons for forecast weather

--- client/src/components/SingleForecast.jsx
+++ client/src/components/SingleForecast.jsx
@@ -1,12 +1,16 @@
 import React from 'react';
 
-const SingleForecast = ({ forecast }) => (
+const SingleForecast = ({ forecast, weatherIcons }) => (
 		<div className="singleForecast">
 			<p className="singleForecast__date">{forecast.day}</p>
+			<img 
+				src={weatherIcons[forecast.weather]} 
+				alt={forecast.weather} 
+				className="singleForecast__icon" 
+			/>
 			<p className="singleForecast__temperature">
 				<span className="singleForecast__low">{forecast.low}</span> / <span className="singleForecast__high">{forecast.high}</span>
 			</p>
-			<p className="singleForecast__weather">{forecast.weather}</p>
 		</div>
 )
 
