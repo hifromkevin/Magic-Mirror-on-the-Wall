dbde096c3610914812a6979c529787f318159515
hifromkevin <lam.kevin86@gmail.com>
2019-08-30 10:28:28 -0700

    Adds API suggestion

--- client/src/config/index.example.js
+++ client/src/config/index.example.js
@@ -1,4 +1,7 @@
 // Remove '.example' from this file name, then enter your API keys below
+// I have included the APIs I used for this application
+// If you choose another API, you may need to update how the data is received
 const config = { 
-  DarkSkyAPI : 'ENTER DARKSKY API KEY HERE' //darksky.net
+  DarkSkyAPI : 'ENTER DARKSKY API KEY HERE', //darksky.net
+  NewsAPI: 'ENTER NEWS API KEY HERE' //newsapi.org
 }
