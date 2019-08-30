27708552b34004fd6853347fc8d3d7e66e2fb1f2
hifromkevin <lam.kevin86@gmail.com>
2019-08-30 10:26:59 -0700

    revises SingleHeadline layout for API call

--- client/src/components/SingleHeadline.jsx
+++ client/src/components/SingleHeadline.jsx
@@ -1,7 +1,10 @@
 import React from 'react';
 
 const SingleHeadline = ({ headline }) => (
-			<p className="singleHeadline">{ headline.title }, By <em>{ headline.author }</em></p>
+			<div className="singleHeadline">
+				<img src="img/news.png" alt={headline.title} className="singleHeadline__icon" />
+				<p><span className="singleHeadline__title">{ headline.title }</span>, By <span className="singleHeadline__author">{ headline.author }</span></p>
+			</div>
 );
 
 export default SingleHeadline;
