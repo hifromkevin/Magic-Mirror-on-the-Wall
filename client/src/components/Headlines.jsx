28c50199f960333c636a519896b925b2e1c91432
hifromkevin <lam.kevin86@gmail.com>
2019-08-30 15:17:32 -0700

    Displays loading message while awaiting information

--- client/src/components/Headlines.jsx
+++ client/src/components/Headlines.jsx
@@ -1,11 +1,16 @@
 import React from 'react';
 import SingleHeadline from './SingleHeadline.jsx';
 
-const Headlines = ({ news }) => (
-	<div className="headlines">
-		<h1 className="headlines__title">Headlines</h1>
-		{news.map((headline, i) => <SingleHeadline headline={headline} key={i} />)}
-	</div>
-)
+const Headlines = ({ news, newsBool }) => {
+	if (newsBool) {
+		return (
+			<div className="headlines">
+				<h1 className="headlines__title">Headlines</h1>
+				{news.map((headline, i) => <SingleHeadline headline={headline} key={i} />)}
+			</div>
+		)
+	}
+  return (<div className="headlines">WAITING FOR HEADLINES</div>)
+}
 
 export default Headlines;
--- client/src/components/Weather.jsx
+++ client/src/components/Weather.jsx
@@ -2,24 +2,29 @@ import React from 'react';
 
 import Forecasts from './Forecasts.jsx';
 
-const Weather = ({ currentWeather, location, forecasts, weatherIcons, days, weatherTranslator }) => (
-		<div className="weather">
-			<p className="weather__temperature">{currentWeather.temperature}</p>
-			<img 
-				src={weatherIcons[weatherTranslator( currentWeather.weather, (new Date(currentWeather.time * 1000).getHours()) )]} 
-				alt={currentWeather.weather} 
-				className="weather__icon" 
-			/>
-			<p className="weather__location">in {location}</p>			
-			<p className="weather__type">{currentWeather.weather}</p>
-			<p className="weather__description">{currentWeather.description}</p>
-			<Forecasts 
-				forecasts={forecasts} 
-				weatherIcons={weatherIcons}
-				weatherTranslator={weatherTranslator}
-				days={days} 
-			/>
-		</div>
-)
+const Weather = ({ currentWeather, location, forecasts, weatherIcons, days, weatherTranslator, weatherBool }) => {
+	if (weatherBool) {
+		return (
+			<div className="weather">
+				<p className="weather__temperature">{currentWeather.temperature}</p>
+				<img 
+					src={weatherIcons[weatherTranslator( currentWeather.weather, (new Date(currentWeather.time * 1000).getHours()) )]} 
+					alt={currentWeather.weather} 
+					className="weather__icon" 
+				/>
+				<p className="weather__location">in {location}</p>			
+				<p className="weather__type">{currentWeather.weather}</p>
+				<p className="weather__description">{currentWeather.description}</p>
+				<Forecasts 
+					forecasts={forecasts} 
+					weatherIcons={weatherIcons}
+					weatherTranslator={weatherTranslator}
+					days={days} 
+				/>
+			</div>
+		)
+	}
+		return (<div className="weather">WAITING FOR WEATHER</div>);
+	}
 
 export default Weather;
