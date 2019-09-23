e261c65db710dbf6e5e1a82e4c0d8f57eed645fe
hifromkevin <lam.kevin86@gmail.com>
2019-09-23 13:04:10 -0700

    Removes ipstack

--- client/src/config/index.example.js
+++ client/src/config/index.example.js
@@ -3,8 +3,7 @@
 // If you choose another API, you may need to update how the data is received
 const config = { 
   DarkSkyAPI : 'ENTER_DARKSKY_API_KEY_HERE', // darksky.net
-  NewsAPI: 'ENTER_NEWS_API_KEY_HERE', // newsapi.org
-  ipstackAPI: 'ENTER_IPSTACK_API_KEY_HERE', // 
+  NewsAPI: 'ENTER_NEWS_API_KEY_HERE' // newsapi.org
 }
 
 module.exports = config;
