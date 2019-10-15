ac86a941013bfb31e5b48db15e2139abcd76eea6
hifromkevin <lam.kevin86@gmail.com>
2019-10-15 12:59:25 -0700

    Reassigns DateAndTime helpers to library

--- client/src/components/DateAndTime.jsx
+++ client/src/components/DateAndTime.jsx
@@ -1,42 +1,18 @@
 import React, { useState, useEffect } from 'react';
 
+import { getTime } from '../lib';
+
 function DateAndTime({ months, days }) {
 	const [currentDate, setCurrentDate] = useState(new Date());
-
-	const getHour = (n) => {
-		if (n > 12) {
-			n = n - 12;
-		} else if (n === 0) {
-			n = 12;
-		} 
-
-		return n;
-	};
-
-	const lessThanTen = (n) => {
-		return (n < 10) ? ('0' + n) : n;
-	}
-
-	const getTime = () => {
-		let second = lessThanTen(currentDate.getSeconds());
-		let minute = lessThanTen(currentDate.getMinutes());
-		let hour = getHour(currentDate.getHours());
-
-		let timeOfDay = (currentDate.getHours() < 12) ? 'AM' : 'PM';
-
-		return `${hour}:${minute}:${second} ${timeOfDay}`;
-	};
 
 	useEffect(() => {
 		setInterval( () => setCurrentDate(new Date()), 1000 );
 	});
  
-
-
 	return (
 		<div className="dateAndTime">
 			{console.log()}
-			<p className="dateAndTime__time">{getTime()}</p>
+			<p className="dateAndTime__time">{getTime(currentDate)}</p>
 			<p className="dateAndTime__date">{days[currentDate.getDay()]}<br /> {months[currentDate.getMonth()]} {currentDate.getDate()}, {currentDate.getFullYear()}</p>
 		</div>
 	);
