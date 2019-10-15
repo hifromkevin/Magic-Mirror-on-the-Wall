707473a3a0b747c98fbc377082656cfc465f087d
hifromkevin <lam.kevin86@gmail.com>
2019-10-15 13:10:22 -0700

    Reallocates helper functions to separate files that are funnelled to lib/index.js

---
+++ client/src/lib/DateAndTimeLib.js
@@ -0,0 +1,25 @@
+const getHour = (n) => {
+  if (n > 12) {
+    n = n - 12;
+  } else if (n === 0) {
+    n = 12;
+  } 
+
+  return n;
+};
+
+const lessThanTen = (n) => {
+  return (n < 10) ? ('0' + n) : n;
+}
+
+const getTime = (currentDate) => {
+  let second = lessThanTen(currentDate.getSeconds());
+  let minute = lessThanTen(currentDate.getMinutes());
+  let hour = getHour(currentDate.getHours());
+
+  let timeOfDay = (currentDate.getHours() < 12) ? 'AM' : 'PM';
+
+  return `${hour}:${minute}:${second} ${timeOfDay}`;
+};
+
+exports.getTime = getTime;
