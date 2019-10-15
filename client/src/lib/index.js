8170fea5aa943901654e2e7660d9527438a3b0ad
hifromkevin <lam.kevin86@gmail.com>
2019-10-15 13:13:05 -0700

    Adds comments

--- client/src/lib/index.js
+++ client/src/lib/index.js
@@ -1,7 +1,9 @@
+// exports for App.jsx
 const AppLib = require('./AppLib');
 exports.apiCalls = AppLib.apiCalls;
 exports.weatherInfo = AppLib.weatherInfo;
 exports.dateInfo = AppLib.dateInfo;
 
+// exports for DateAndTime.jsx
 const DateAndTimeLib = require('./DateAndTimeLib');
 exports.getTime = DateAndTimeLib.getTime;
