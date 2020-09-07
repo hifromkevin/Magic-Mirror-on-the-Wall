244b6f730ec64375a87dbbb810491e254622ab71
Kevin Lam <lam.kevin86@gmail.com>
2020-09-07 14:20:42 -0700

    Swaps out Darksky for WeatherBitAPI

--- client/src/config/index.example.js
+++ client/src/config/index.example.js
@@ -1,10 +1,9 @@
 // Remove '.example' from this file name, then enter your API keys below
 // I have included the APIs I used for this application
 // If you choose another API, you may need to update how the data is received
-const config = { 
-  DarkSkyAPI : 'ENTER_DARKSKY_API_KEY_HERE', // darksky.net
-  NewsAPI: 'ENTER_NEWS_API_KEY_HERE' // newsapi.org
-}
+const config = {
+  WeatherBitAPI: "ENTER_WEATHERBIT_API_KEY_HERE", //https://www.weatherbit.io/
+  NewsAPI: "ENTER_NEWS_API_KEY_HERE", // newsapi.org
+};
 
 module.exports = config;
-
