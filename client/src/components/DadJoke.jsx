1dda7efcf4bebcfef0df3eee4c5abbc9d188cbba
hifromkevin <lam.kevin86@gmail.com>
2021-10-17 17:25:45 -0700

    Renames 'WelcomeText' to 'DadJoke'

---
+++ client/src/components/DadJoke.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const DadJoke = ({ dadJoke }) => (
+	<div className="dadJoke">
+		<p className="dadJoke__text">{dadJoke}</p>
+	</div>
+)
+
+export default DadJoke;
--- client/src/components/WelcomeText.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const WelcomeText = ({ dadJoke }) => (
-	<div className="welcomeText">
-		<p className="welcomeText__text">{dadJoke}</p>
-	</div>
-)
-
-export default WelcomeText;
