ea6deb8f124212c354d090994111931d6857bc56
hifromkevin <lam.kevin86@gmail.com>
2023-06-22 23:25:36 -0700

    Removes 'by' if no author is present

--- client/src/components/SingleHeadline.jsx
+++ client/src/components/SingleHeadline.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const SingleHeadline = ({ headline }) => (
 	<div className="singleHeadline">
 		<img src="img/news.png" alt={headline.title} className="singleHeadline__icon" />
-		<span><span className="singleHeadline__title">{headline.title}</span>, By <span className="singleHeadline__author">{headline.author}</span></span>
+		<span><span className="singleHeadline__title">{headline.title}</span>{headline.author ? ', By' : ''} <span className="singleHeadline__author">{headline.author}</span></span>
 	</div>
 );
 
