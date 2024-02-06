0d0b8e9d2d634651ba7f87a08c4e9e705d76a2f3
hifromkevin <lam.kevin86@gmail.com>
2024-02-06 14:45:44 -0800

    Refactors configs

--- server/index.js
+++ server/index.js
@@ -3,29 +3,29 @@ const app = express();
 const axios = require('axios');
 const port = 3005;
 
-const config = require('../client/src/config/index');
+const { AccuWeatherAPI, ipInfoAPI, NewsAPI } = require('../client/src/config/index');
 
 app.use(express.static(__dirname + '/../client/dist'));
 
 app.get('/location', (req, res) => {
-  axios.get(`https://ipinfo.io/json?token=${config.ipInfoAPI}`)
+  axios.get(`https://ipinfo.io/json?token=${ipInfoAPI}`)
     .then(async (data) => {
       const {
         data: { city, region, postal }
       } = data;
 
       const getLocationCode = await axios.get(
-        `http://dataservice.accuweather.com/locations/v1/postalcodes/search?apikey=${config.AccuWeatherAPI}&q=${postal}`
+        `http://dataservice.accuweather.com/locations/v1/postalcodes/search?apikey=${AccuWeatherAPI}&q=${postal}`
       );
 
       const [locationData] = getLocationCode.data;
 
       const weatherForecast = await axios.get(
-        `http://dataservice.accuweather.com/forecasts/v1/daily/5day/${locationData.Key}?apikey=${config.AccuWeatherAPI}`
+        `http://dataservice.accuweather.com/forecasts/v1/daily/5day/${locationData.Key}?apikey=${AccuWeatherAPI}`
       );
 
       const currentWeather = await axios.get(
-        `http://dataservice.accuweather.com/currentconditions/v1/${locationData.Key}?apikey=${config.AccuWeatherAPI}`
+        `http://dataservice.accuweather.com/currentconditions/v1/${locationData.Key}?apikey=${AccuWeatherAPI}`
       );
 
       res.status(200).send({
@@ -39,7 +39,7 @@ app.get('/location', (req, res) => {
 });
 
 app.get('/news', (req, res) => {
-  axios.get(`https://newsapi.org/v2/top-headlines?country=us&apiKey=${config.NewsAPI}`)
+  axios.get(`https://newsapi.org/v2/top-headlines?country=us&apiKey=${NewsAPI}`)
     .then(data => res.status(200).send({ data: data.data.articles }))
     .catch(err => console.log('No news is bad news :(', err));
 });
