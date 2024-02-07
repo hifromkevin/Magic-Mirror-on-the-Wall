3d6e85ccb5d933ec9531717c8bb0189cd0c2fb16
hifromkevin <lam.kevin86@gmail.com>
2024-02-06 16:33:53 -0800

    Modernizes Webpack

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,7 +1,11 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { createRoot } from 'react-dom/client';
 
 import App from './components/App.jsx';
 import '../dist/styles/style.sass';
 
-ReactDOM.render(<App />, document.getElementById('app'));
+const root = createRoot(document.getElementById('app'));
+
+root.render(<React.StrictMode>
+  <App />
+</React.StrictMode>);
--- webpack.config.js
+++ webpack.config.js
@@ -1,40 +1,31 @@
 const path = require('path');
-const DIST_DIR = path.join(__dirname, '/client/dist'); 
+const DIST_DIR = path.join(__dirname, '/client/dist');
 const SRC_DIR = path.join(__dirname, '/client/src');
 
 module.exports = {
 	mode: 'development',
-	entry: `${SRC_DIR}/index.jsx`,
+
+	entry: {
+		main: `${SRC_DIR}/index.jsx`,
+	},
 	output: {
 		path: DIST_DIR,
 		filename: 'bundle.js'
 	},
+
 	module: {
 		rules: [
 			{
-        test: /\.jsx?$/, 
-        resolve: {
-          extensions: [".js", ".jsx"],
-          enforceExtension: false
-        },
-				query: {
-					presets: ['@babel/preset-env', '@babel/preset-react'],
-					plugins: ['@babel/plugin-proposal-class-properties']
-        },
-          loader: 'babel-loader'
+				test: /\.(js|jsx)$/,
+				exclude: /nodeModules/,
+				use: {
+					loader: 'babel-loader'
+				}
 			},
 			{
 				test: /\.s[ac]ss$/i,
-				loaders: ['style-loader','css-loader','sass-loader']
-			},
-      {
-        test: /\.css$/,
-        loaders: ['style-loader','css-loader','sass-loader']
-      },
-      {
-        test: /\.(png|woff|woff2|eot|ttf|svg)$/,
-        loader: 'url-loader?limit=100000'
-      }
+				use: ['style-loader', 'css-loader', 'sass-loader']
+			}
 		]
-  }
+	}
 };
