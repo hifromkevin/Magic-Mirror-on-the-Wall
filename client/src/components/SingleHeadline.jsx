a6453dbe191cdf614a963b9780e905bc6138f4a9
hifromkevin <lam.kevin86@gmail.com>
2025-02-04 23:53:07 -0800

    Destructures props

--- client/src/components/SingleHeadline.jsx
+++ client/src/components/SingleHeadline.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
 
-const SingleHeadline = ({ headline }) => (
+const SingleHeadline = ({ headline: { title, author } }) => (
 	<div className="singleHeadline">
-		<img src="img/news.png" alt={headline.title} className="singleHeadline__icon" />
-		<span><span className="singleHeadline__title">{headline.title}</span><span className="singleHeadline__author">{headline.author ? ', By ' : ''}{headline.author}</span></span>
+		<img src="img/news.png" alt={title} className="singleHeadline__icon" />
+		<span><span className="singleHeadline__title">{title}</span><span className="singleHeadline__author">{author ? ', By ' : ''}{author}</span></span>
 	</div>
 );
 
