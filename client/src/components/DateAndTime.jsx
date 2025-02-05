590d3be0cdceea4ca25774208e90460d05d11bef
hifromkevin <lam.kevin86@gmail.com>
2025-02-04 23:50:58 -0800

    Cleans up date functionality

--- client/src/components/DateAndTime.jsx
+++ client/src/components/DateAndTime.jsx
@@ -3,19 +3,25 @@ import React, { useState, useEffect } from 'react';
 import { getTime } from '../lib';
 
 const DateAndTime = (props) => {
-	const [currentDate, setCurrentDate] = useState(new Date());
+  const [currentDate, setCurrentDate] = useState(new Date());
 
-	useEffect(() => {
-		setInterval(() => setCurrentDate(new Date()), 1000);
-	});
+  useEffect(() => {
+    const intervalId = setInterval(() => setCurrentDate(new Date()), 1000);
+    return () => clearInterval(intervalId);
+  }, []);
 
-	return (
-		<div className="dateAndTime">
-			<p className="dateAndTime__time">{getTime(currentDate)}</p>
-			<p className="dateAndTime__date">{currentDate.toLocaleString('default', { month: 'long' })} {currentDate.getDate()}, {currentDate.getFullYear()}</p>
-			<p className="dateAndTime__date">{currentDate.toLocaleString("en-US", { weekday: 'long' })}</p>
-		</div>
-	);
+  return (
+    <div className="dateAndTime">
+      <p className="dateAndTime__time">{getTime(currentDate)}</p>
+      <p className="dateAndTime__date">
+        {currentDate.toLocaleString('en-US', { month: 'long' })}{' '}
+        {currentDate.getDate()}, {currentDate.getFullYear()}
+      </p>
+      <p className="dateAndTime__date">
+        {currentDate.toLocaleString('en-US', { weekday: 'long' })}
+      </p>
+    </div>
+  );
 };
 
 export default DateAndTime;
