fff432a5088667ef90218fc74ac5385a1caeaa4b
hifromkevin <lam.kevin86@gmail.com>
2025-02-04 23:51:52 -0800

    Cleans up state

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,9 +1,10 @@
-import "babel-polyfill";
-import React, { useState, useEffect } from "react";
+import 'babel-polyfill';
+import React, { useState, useEffect } from 'react';
 
-import MirrorUi from "./MirrorUi.jsx";
+import MirrorUi from './MirrorUi.jsx';
 
-const SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition;
+const SpeechRecognition =
+  window.SpeechRecognition || window.webkitSpeechRecognition;
 const mic = new SpeechRecognition();
 
 mic.start();
@@ -12,32 +13,22 @@ mic.interimResults = true;
 mic.lang = 'en-US';
 
 const App = () => {
-  const [
-    setUi,
-    setUiInfo
-  ] = useState({
-    selectedPage: 'mirror'
-  });
+  const [selectedPage, setSelectedPage] = useState('');
 
   const voiceControl = () => {
     mic.onspeechstart = () => {
       console.log('Speech started.');
     };
 
-    mic.onresult = e => {
+    mic.onresult = (e) => {
       let current = e.resultIndex;
 
       const transcript = e.results[current][0].transcript;
       console.log('Spoken word: ', transcript);
 
       if (transcript === 'magic mirror on the wall') {
-        setUiInfo(
-          state => ({
-            ...state,
-            selectedPage: 'mirror'
-          })
-        );
-      };
+        setSelectedPage('mirror');
+      }
 
       mic.onspeechend = () => {
         mic.stop();
@@ -46,11 +37,11 @@ const App = () => {
     };
   };
 
-  useEffect(voiceControl);
+  useEffect(voiceControl, []);
 
-  if (setUi.selectedPage === 'mirror') return <MirrorUi />;
+  if (selectedPage === 'mirror') return <MirrorUi />;
 
-  return <div className='main'>&nbsp;</div>;
+  return <div className="main">&nbsp;</div>;
 };
 
 export default App;
