20685d5431ef0aadf65c5e6156c49580658f2134
hifromkevin <lam.kevin86@gmail.com>
2025-02-07 22:35:24 -0800

    Adds animated color-changing border

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -3,45 +3,49 @@ import React, { useState, useEffect } from 'react';
 
 import MirrorUi from './MirrorUi.jsx';
 
-const SpeechRecognition =
-  window.SpeechRecognition || window.webkitSpeechRecognition;
-const mic = new SpeechRecognition();
+// const SpeechRecognition =
+//   window.SpeechRecognition || window.webkitSpeechRecognition;
+// const mic = new SpeechRecognition();
 
-mic.start();
-mic.continuous = true;
-mic.interimResults = true;
-mic.lang = 'en-US';
+// mic.start();
+// mic.continuous = true;
+// mic.interimResults = true;
+// mic.lang = 'en-US';
 
 const App = () => {
-  const [selectedPage, setSelectedPage] = useState('');
+  const [selectedPage, setSelectedPage] = useState('mirror');
 
-  const voiceControl = () => {
-    mic.onspeechstart = () => {
-      console.log('Speech started.');
-    };
+  // const voiceControl = () => {
+  //   mic.onspeechstart = () => {
+  //     console.log('Speech started.');
+  //   };
 
-    mic.onresult = (e) => {
-      let current = e.resultIndex;
+  //   mic.onresult = (e) => {
+  //     let current = e.resultIndex;
 
-      const transcript = e.results[current][0].transcript;
-      console.log('Spoken word: ', transcript);
+  //     const transcript = e.results[current][0].transcript;
+  //     console.log('Spoken word: ', transcript);
 
-      if (transcript === 'magic mirror on the wall') {
-        setSelectedPage('mirror');
-      }
+  //     if (transcript === 'magic mirror on the wall') {
+  //       setSelectedPage('mirror');
+  //     }
 
-      mic.onspeechend = () => {
-        mic.stop();
-        console.log('Voice stopped.');
-      };
-    };
-  };
+  //     mic.onspeechend = () => {
+  //       mic.stop();
+  //       console.log('Voice stopped.');
+  //     };
+  //   };
+  // };
 
-  useEffect(voiceControl, []);
+  // useEffect(voiceControl, []);
 
-  if (selectedPage === 'mirror') return <MirrorUi />;
-
-  return <div className="main">&nbsp;</div>;
+  return (
+    <div className="appContainer">
+      <div className="appSubContainer">
+        {selectedPage === 'mirror' ? <MirrorUi /> : <p></p>}
+      </div>
+    </div>
+  );
 };
 
 export default App;
--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,14 +1,14 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 
-// import App from './components/App.jsx';
-import MirrorUi from './components/MirrorUi.jsx';
+import App from './components/App.jsx';
+// import MirrorUi from './components/MirrorUi.jsx';
 import '../dist/styles/style.sass';
 
 const root = createRoot(document.getElementById('app'));
 
 root.render(
   <React.StrictMode>
-    <MirrorUi />
+    <App />
   </React.StrictMode>
 );
