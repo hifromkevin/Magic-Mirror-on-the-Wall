4c153cc09336e76e18ee533ba24cb9c45361437c
hifromkevin <lam.kevin86@gmail.com>
2025-02-18 00:04:41 -0800

    Adds simple tests

--- client/tests/DadJoke.test.jsx
+++ client/tests/DadJoke.test.jsx
@@ -3,8 +3,14 @@ import { render, screen } from '@testing-library/react';
 import '@testing-library/jest-dom';
 import DadJoke from '../src/components/DadJoke';
 
-test('Dad Joke', async () => {
+test('Dad joke receives text', async () => {
   render(<DadJoke dadJoke={"Hi testing suite, I'm dad!"} />);
   const jokeElement = screen.getByText("Hi testing suite, I'm dad!");
   expect(jokeElement).toHaveTextContent("Hi testing suite, I'm dad!");
+});
+
+test('Dad joke classname is applied', async () => {
+  render(<DadJoke dadJoke={"Hi testing suite, I'm dad!"} />);
+  const dadJoke = screen.getByText("Hi testing suite, I'm dad!");
+  expect(dadJoke).toHaveClass('dadJoke');
 });
