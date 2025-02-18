244da1eb5d2f273d2d305b9d3bc922cb98f66caa
hifromkevin <lam.kevin86@gmail.com>
2025-02-17 23:43:48 -0800

    Sets up testing

---
+++ jest.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  collectCoverage: true,
+  collectCoverageFrom: ['src/**/*.{js,jsx}'],
+  coverageDirectory: 'coverage',
+  testEnvironment: 'jsdom',
+  testPathIgnorePatterns: ['/node_modules/'],
+  moduleNameMapper: {
+    '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
+      '<rootDir>/__mocks__/fileMock.js',
+    '\\.(css|less|scss)$': '<rootDir>/__mocks__/styleMock.js',
+  },
+};
