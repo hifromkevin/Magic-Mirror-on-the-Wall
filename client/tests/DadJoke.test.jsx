14dfe1e88327d6ec60398b34a1db866d66f3e4ab
hifromkevin <lam.kevin86@gmail.com>
2025-02-19 16:22:27 -0800

    Adds testing content

--- client/tests/DadJoke.test.jsx
+++ client/tests/DadJoke.test.jsx
@@ -3,14 +3,16 @@ import { render, screen } from '@testing-library/react';
 import '@testing-library/jest-dom';
 import DadJoke from '../src/components/DadJoke';
 
-test('Dad joke receives text', async () => {
-  render(<DadJoke dadJoke={"Hi testing suite, I'm dad!"} />);
-  const jokeElement = screen.getByText("Hi testing suite, I'm dad!");
-  expect(jokeElement).toHaveTextContent("Hi testing suite, I'm dad!");
-});
+describe('Dad Jokes', () => {
+  test('Dad joke receives text', async () => {
+    render(<DadJoke dadJoke={"Hi testing suite, I'm dad!"} />);
+    const jokeElement = screen.getByText("Hi testing suite, I'm dad!");
+    expect(jokeElement).toHaveTextContent("Hi testing suite, I'm dad!");
+  });
 
-test('Dad joke classname is applied', async () => {
-  render(<DadJoke dadJoke={"Hi testing suite, I'm dad!"} />);
-  const dadJoke = screen.getByText("Hi testing suite, I'm dad!");
-  expect(dadJoke).toHaveClass('dadJoke');
+  test('Dad joke classname is applied', async () => {
+    render(<DadJoke dadJoke={"Hi testing suite, I'm dad!"} />);
+    const dadJoke = screen.getByText("Hi testing suite, I'm dad!");
+    expect(dadJoke).toHaveClass('dadJoke');
+  });
 });
--- client/tests/SingleHeadline.test.jsx
+++ client/tests/SingleHeadline.test.jsx
@@ -3,48 +3,50 @@ import { render, screen } from '@testing-library/react';
 import '@testing-library/jest-dom';
 import SingleHeadline from '../src/components/SingleHeadline';
 
-test('Single Headline with author', async () => {
-  render(
-    <SingleHeadline
-      headline={{ title: 'This is a title', author: 'Jones, Bob' }}
-    />
-  );
-  const displaysTitle = screen.getByText('This is a title');
-  const displaysAuthor = screen.getByText(', By Jones, Bob');
-  expect(displaysTitle).toHaveTextContent('This is a title');
-  expect(displaysAuthor).toHaveTextContent(', By Jones, Bob');
-});
-
-test('Single Headline without author', async () => {
-  render(
-    <SingleHeadline headline={{ title: 'This is a title', author: '' }} />
-  );
-  const displaysTitle = screen.getByText('This is a title');
-  const authorElement = screen.getByText((content, element) => {
-    return element.className === 'singleHeadline__author' && content === '';
+describe('Single Headline', () => {
+  test('Single Headline with author', async () => {
+    render(
+      <SingleHeadline
+        headline={{ title: 'This is a title', author: 'Jones, Bob' }}
+      />
+    );
+    const displaysTitle = screen.getByText('This is a title');
+    const displaysAuthor = screen.getByText(', By Jones, Bob');
+    expect(displaysTitle).toHaveTextContent('This is a title');
+    expect(displaysAuthor).toHaveTextContent(', By Jones, Bob');
   });
-  expect(displaysTitle).toHaveTextContent('This is a title');
-  expect(authorElement).toBeEmptyDOMElement();
-});
 
-test('News icon appears with correct alt text', async () => {
-  render(
-    <SingleHeadline
-      headline={{ title: 'This is a title', author: 'Jones, Bob' }}
-    />
-  );
-  const image = screen.getByAltText('This is a title');
-  expect(image).toBeInTheDocument();
-});
+  test('Single Headline without author', async () => {
+    render(
+      <SingleHeadline headline={{ title: 'This is a title', author: '' }} />
+    );
+    const displaysTitle = screen.getByText('This is a title');
+    const authorElement = screen.getByText((content, element) => {
+      return element.className === 'singleHeadline__author' && content === '';
+    });
+    expect(displaysTitle).toHaveTextContent('This is a title');
+    expect(authorElement).toBeEmptyDOMElement();
+  });
 
-test('Correct classnames are applied', async () => {
-  render(
-    <SingleHeadline
-      headline={{ title: 'This is a title', author: 'Jones, Bob' }}
-    />
-  );
-  const displaysTitle = screen.getByText('This is a title');
-  const displaysAuthor = screen.getByText(', By Jones, Bob');
-  expect(displaysTitle).toHaveClass('singleHeadline__title');
-  expect(displaysAuthor).toHaveClass('singleHeadline__author');
+  test('News icon appears with correct alt text', async () => {
+    render(
+      <SingleHeadline
+        headline={{ title: 'This is a title', author: 'Jones, Bob' }}
+      />
+    );
+    const image = screen.getByAltText('This is a title');
+    expect(image).toBeInTheDocument();
+  });
+
+  test('Correct classnames are applied', async () => {
+    render(
+      <SingleHeadline
+        headline={{ title: 'This is a title', author: 'Jones, Bob' }}
+      />
+    );
+    const displaysTitle = screen.getByText('This is a title');
+    const displaysAuthor = screen.getByText(', By Jones, Bob');
+    expect(displaysTitle).toHaveClass('singleHeadline__title');
+    expect(displaysAuthor).toHaveClass('singleHeadline__author');
+  });
 });
